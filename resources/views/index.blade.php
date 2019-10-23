bd791f1c844268911f93a880e3b2a5af26b081ad
Vlad <azvart1994@gmail.com>
2019-10-23 20:58:53 +0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Welcome to site</div>
+
+                <div class="card-body">
+                   Добро пожаловать на сайт!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/static.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.base')
+
+@section('content')
+<h2>{{$url}}</h2>
+<div>
+main text
+</div>
+@endsection
